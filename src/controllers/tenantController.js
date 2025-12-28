15ece3e43905ea5d5d5149428db518a0e349efb7
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-12-28 12:42:18 +0530

    Refactor tenantController for clarity and efficiency

--- src/controllers/tenantController.js
+++ src/controllers/tenantController.js
@@ -1,7 +1,7 @@
 const Property = require('../models/Property');
 const Request = require('../models/Request');
 
-// Explore properties
+
 exports.exploreProperties = async (req, res) => {
   try {
     const properties = await Property.find()
@@ -13,7 +13,7 @@ exports.exploreProperties = async (req, res) => {
   }
 };
 
-// Apply for property - ULTRA CLEAN
+
 exports.applyForProperty = async (req, res) => {
   try {
     const { propertyId } = req.body;
@@ -21,20 +21,18 @@ exports.applyForProperty = async (req, res) => {
     console.log('🔍 PropertyId:', propertyId);
     console.log('🔍 UserId:', req.user?._id);
 
-    // Validation
     if (!propertyId) return res.status(400).json({ message: 'Property ID required' });
     if (!req.user?._id) return res.status(401).json({ message: 'Login required' });
 
-    // Find property
+ 
     const property = await Property.findById(propertyId);
     if (!property) return res.status(404).json({ message: 'Property not found' });
 
-    // Check owner
+ 
     if (property.owner.toString() === req.user._id.toString()) {
       return res.status(400).json({ message: 'Cannot apply to own property' });
     }
 
-    // Check existing request
     const existingRequest = await Request.findOne({
       property: propertyId,
       tenant: req.user._id
@@ -43,7 +41,6 @@ exports.applyForProperty = async (req, res) => {
       return res.status(400).json({ message: 'Already applied' });
     }
 
-    // Create request
     const request = new Request({
       property: propertyId,
       tenant: req.user._id,
@@ -51,7 +48,7 @@ exports.applyForProperty = async (req, res) => {
     });
     await request.save();
 
-    // Update property
+
     property.requests.push(request._id);
     await property.save();
 
@@ -64,7 +61,7 @@ exports.applyForProperty = async (req, res) => {
   }
 };
 
-// Get applications
+
 exports.getTenantApplications = async (req, res) => {
   try {
     const requests = await Request.find({ tenant: req.user._id })
